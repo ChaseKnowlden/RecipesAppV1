b2a737415cc775e231fe9fcc714b273e42545b8c
ChaseKnowlden <knowlden887@gmail.com>
2019-06-13 11:36:28 -0400

    Updates

---
+++ precache-manifest.61cb5713216854349923a14be9fb8213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d6b3070c7a94ed69dd9073727e8119",
+    "url": "/recipeapprj/index.html"
+  },
+  {
+    "revision": "a4809ec80accd43725e9",
+    "url": "/recipeapprj/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3374b626c4cbffc79b0e",
+    "url": "/recipeapprj/static/css/main.c94d901d.chunk.css"
+  },
+  {
+    "revision": "a4809ec80accd43725e9",
+    "url": "/recipeapprj/static/js/2.32effade.chunk.js"
+  },
+  {
+    "revision": "3374b626c4cbffc79b0e",
+    "url": "/recipeapprj/static/js/main.254a1a1f.chunk.js"
+  },
+  {
+    "revision": "9ff8f210c46c8bd764b4",
+    "url": "/recipeapprj/static/js/runtime~main.a112225a.js"
+  }
+]);
